4e24866724023f8027f3e946596fd3e9e93dfb19
David-ZemaP <david.zema@ucb.edu.bo>
2025-09-11 23:46:31 -0400

    feat: parte del dominio

--- app/src/main/java/com/calyrsoft/ucbp1/features/movies/data/api/MoviesService.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/movies/data/api/MoviesService.kt
@@ -1,11 +1,12 @@
 package com.calyrsoft.ucbp1.features.movies.data.api
 
-import com.calyrsoft.ucbp1.features.github.data.api.dto.GithubDto
+import com.calyrsoft.ucbp1.features.movies.data.api.dto.MoviesDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MoviesService {
-    @GET("/3/discover/movie?sort_by=popularity.desc&api_key=fa3e844ce31744388e07fa47c7c5d8c3")
-    suspend fun getInfoAvatar(@Path("githubLogin") githubLogin: String): Response<GithubDto>
+    @GET("/3/discover/movie")
+    suspend fun getPopularMovies(@Query("sort_by")sortBy:String,
+                                 @Query("api_key")apiKey:String): Response<MoviesDto>
 }
--- app/src/main/java/com/calyrsoft/ucbp1/features/movies/data/datasource/MoviesRemoteDataSource.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/movies/data/datasource/MoviesRemoteDataSource.kt
@@ -1,4 +1,16 @@
 package com.calyrsoft.ucbp1.features.movies.data.datasource
 
-class MoviesRemoteDataSource {
+import com.calyrsoft.ucbp1.features.movies.data.api.MoviesService
+import com.calyrsoft.ucbp1.features.movies.data.api.dto.MoviesDto
+
+class MoviesRemoteDataSource (val movieservice: MoviesService) {
+    suspend fun getMovie(id:Number): Result<MoviesDto> {
+        val response = movieservice.getPopularMovies("popularity.desc", "fa3e844ce31744388e07fa47c7c5d8c3")
+
+        if(response.isSuccessful) {
+            return Result.success(response.body()!!)
+        } else {
+            return Result.failure(Exception("Error al obtener el usuario"))
+        }
+    }
 }
